c3b3f01e49f3c02c03d6bb93fa9f41c09e9bc54e
mimex <mimex@fipasoft.mx>
2015-03-12 15:12:29 -0600

    RFT: Mensajes para aclarar el estado actual del repo

--- gensetup-admincfdi.sh
+++ gensetup-admincfdi.sh
@@ -4,8 +4,10 @@ repo='https://github.com/linuxcabal/admin-cfdi'
 function getrepo {
     if [ ! -f ./admin-cfdi/admincfdi.py ]
     then
+        "Clonando "${repo}
         git clone ${repo}
     else
+        "Actualizando desde "${repo}
         cd ./admin-cfdi
         git pull origin master
         cd ..
