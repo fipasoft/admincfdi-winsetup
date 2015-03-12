f15c550380771360b4732ee4b4ec8e316945e795
mimex <mimex@fipasoft.mx>
2015-03-12 14:55:40 -0600

    RFT: Funcion para actualizar el repo de admin-cfdi

--- gensetup-admincfdi.sh
+++ gensetup-admincfdi.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 repo='https://github.com/linuxcabal/admin-cfdi'
 
-if [ ! -f ./admin-cfdi/admincfdi.py ]
-then
-    git clone ${repo}
-fi
+function getrepo {
+    if [ ! -f ./admin-cfdi/admincfdi.py ]
+    then
+        git clone ${repo}
+    else
+        cd ./admin-cfdi
+        git pull origin master
+        cd ..
+    fi
+}
+
+getrepo
